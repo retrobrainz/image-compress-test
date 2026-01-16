2637bfbe33a7cc59759be3c66b0151b3c74f5b69
Guo Yunhe <i@guoyunhe.me>
2026-01-17 00:29:55 +0800

    compress

---
+++ compress.js
@@ -0,0 +1,17 @@
+import sharp from "sharp";
+import { join } from "node:path";
+
+sharp(join(import.meta.dirname, "input.png"))
+  .png({
+    palette: true,
+    effort: 10,
+  })
+  .toFile(join(import.meta.dirname, "output.png"));
+
+sharp(join(import.meta.dirname, "input.png"))
+  .webp({})
+  .toFile(join(import.meta.dirname, "output.webp"));
+
+sharp(join(import.meta.dirname, "input.png"))
+  .avif({})
+  .toFile(join(import.meta.dirname, "output.avif"));
